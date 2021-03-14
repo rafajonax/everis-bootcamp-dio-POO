bc287fccb6a46755b7ef1acd7beacb18e1731910
Rafael Jonas <rafajonax@gmail.com>
2021-03-14 20:29:37 -0300

    Adiciona o método pessoaInfo a classe pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,11 +5,14 @@ class Pessoa {
 
     var cpf: String = "999.999.999.00"
     private set
+
+    fun pessoaInfo() = "Nome: $nome - CPF: $cpf"
 }
 
 fun main() {
     val rafael = Pessoa()
 
-    println(rafael.nome)
-    println(rafael.cpf)
+//    println(rafael.nome)
+//    println(rafael.cpf)
+    println(rafael.pessoaInfo())
 }
