f207b0c198c058f7864ba24e38347da888eda252
Rafael Jonas <rafajonax@gmail.com>
2021-03-15 23:48:37 -0300

    Atualizadas classe Gerente.kt e TesteFuncionarioGerente.kt implantando método de autenticação

--- src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
@@ -3,7 +3,10 @@ package one.digitalinnovation.digionebank
 class Gerente(
     nome: String,
     cpf: String,
-    salario: Double
-) : Funcionario(nome, cpf, salario) {
+    salario: Double,
+    val senha: String
+) : Funcionario(nome, cpf, salario), Logavel {
     override fun calculoAuxilio(): Double = salario * .5
+
+    override fun login(): Boolean = "minhasenha" == senha
 }
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,9 +4,16 @@ import one.digitalinnovation.digionebank.Funcionario
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val heloisa = Gerente(nome = "Heloisa Fernandez", cpf = "34356577866", salario = 7300.00)
+    val heloisa = Gerente(
+        nome = "Heloisa Fernandez",
+        cpf = "34356577866",
+        salario = 7300.00,
+        senha = "minhasenha"
+    )
 
     ImprimeRelatorioFuncionario.imprime(heloisa)
+
+    TesteAutenticacao().autentica(heloisa)
 }
 
 //fun imprimeRelatorio(funcionario: Funcionario): String = println(funcionario.toString())
